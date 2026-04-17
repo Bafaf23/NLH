d4844183603023e3431060bf2d8d19dcc2d45938
Bryant Facenda <bryantffacen@gmail.com>
2026-04-16 21:58:33 -0400

    Enhance user authentication and registration features. Added password hashing, improved error handling, and implemented a login route. Updated UI for better user experience and added new CSS styles for scrollbar customization.

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+from cProfile import label
 from os import name
-from flask import Flask, render_template, template_rendered, request, session
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    template_rendered,
+    request,
+    session,
+    url_for,
+)
 import pymysql
 from datetime import datetime
 from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = "estoy_cansado_jefe"
@@ -43,60 +53,105 @@ def home():
         },
     ]
 
-    return render_template("index.html", saludo=saludo, user=None, news=news)
+    return render_template("index.html", saludo=saludo, news=news)
 
 
 @app.route("/auth", methods=["GET", "POST"])
 def register():
-    if (
-        request.method == "POST"
-        and "nameUser" in request.form
-        and "password" in request.form
-    ):
-        typeDocument = request.form["typeDocument"]
-        document = request.form["document"]
-        nameUser = request.form["nameUser"]
-        lastName = request.form["lastName"]
-        email = request.form["email"]
-        birthdate = request.form["birthdate"]
-        password = request.form["password"]
-        confirm_password = request.form["confirm_password"]
+    try:
+        if (
+            request.method == "POST"
+            and "nameUser" in request.form
+            and "password" in request.form
+        ):
+            typeDocument = request.form["typeDocument"]
+            document = request.form["document"]
+            nameUser = request.form["nameUser"]
+            lastName = request.form["lastName"]
+            email = request.form["email"]
+            birthdate = request.form["birthdate"]
+            password = request.form["password"]
+            confirm_password = request.form["confirm_password"]
 
-        dni = f"{typeDocument}{document}"
+            dni = f"{typeDocument}{document}"
 
-        if confirm_password != password:
-            print("las claves no son iguales")
-            return render_template("auth/register.html")
+            if confirm_password != password:
+                print("las claves no son iguales")
+                return render_template("auth/register.html")
 
-        conn = mysql.connect()
-        cur = conn.cursor(pymysql.cursors.DictCursor)
+            conn = mysql.connect()
+            cur = conn.cursor(pymysql.cursors.DictCursor)
 
-        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
-        account = cur.fetchone()
+            cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+            account = cur.fetchone()
 
-        if account:
-            print("El usuario ya esta registrado")
-            return render_template("auth/register.html")
+            if account:
+                print("El usuario ya esta registrado")
+                return render_template("auth/register.html")
 
-        else:
-            id_role_default = 2
-            sql = "INSERT INTO users (dni, name, last_Name, email, birthdate, pass, role_id) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+            else:
+                id_role_default = 2
+                password_encriptada = generate_password_hash(password)
+                sql = "INSERT INTO users (dni, name, last_Name, email, birthdate, pass, role_id) VALUES(%s,%s,%s,%s,%s,%s,%s)"
 
-            valores = (
-                dni,
-                nameUser,
-                lastName,
-                email,
-                birthdate,
-                password,
-                id_role_default,
-            )
-            print(valores)
-            cur.execute(sql, valores)
-            conn.commit()
-            print("You have successfully registered!")
+                valores = (
+                    dni,
+                    nameUser,
+                    lastName,
+                    email,
+                    birthdate,
+                    password_encriptada,
+                    id_role_default,
+                )
+                print(valores)
+                cur.execute(sql, valores)
+                conn.commit()
+                print("You have successfully registered!")
+
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
 
     return render_template("auth/register.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    try:
+        if (
+            request.method == "POST"
+            and "email" in request.form
+            and "pass" in request.form
+        ):
+            email = request.form["email"]
+            password = request.form["pass"]
+
+            conn = mysql.connect()
+            cur = conn.cursor(pymysql.cursors.DictCursor)
+
+            sql = "SELECT * FROM users WHERE email = %s AND pass = %s"
+            valor = (email, password)
+
+            cur.execute(sql, valor)
+
+            user = cur.fetchone()
+
+            if user:
+                session["loggedin"] = True
+                session["id"] = user["id"]
+                session["name"] = user["name"]
+                session["lastName"] = user["last_name"]
+                session["email"] = user["email"]
+                session["role"] = user["role_id"]
+
+                print(f"Sesión iniciada para: {user['name']}")
+                return redirect(url_for("home"))
+            else:
+                print("Usuario o clave errada")
+
+    except Exception as e:
+        print(f"hubo un error {e}")
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
